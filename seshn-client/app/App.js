20973fb727243162dbbf4eff3fc265e31f3cb047
toehmler <roehmler@middlebury.edu>
2022-07-25 11:48:05 -0700

    add basic ui for api demo

--- seshn-client/app/App.js
+++ seshn-client/app/App.js
@@ -1,15 +1,51 @@
 // app.js
 
-import React, {useState} from 'react';
-import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  View,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import Auth0 from 'react-native-auth0';
+import axios from 'axios';
+import UserPostList from './UserPostList';
+import {Text} from '@rneui/themed';
 
-var credentials = require('./auth0-config');
+let credentials = require('./auth0-config');
 
 const auth0 = new Auth0(credentials);
 
 const App = () => {
   let [accessToken, setAccessToken] = useState(null);
+  let [inputText, setInputText] = useState(null);
+  let [bodyText, setBodyText] = useState(null);
+  let [userPosts, setUserPosts] = useState([]);
+
+  let loggedIn = accessToken !== null;
+
+  const fetchUserPosts = () => {
+    if (!loggedIn) {
+      return;
+    }
+    axios
+      .get('http://localhost:5004/posts/', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(response => {
+        console.log(response.data);
+        setUserPosts(response.data.reverse());
+      });
+  };
+
+  useEffect(() => {
+    fetchUserPosts();
+  }, [accessToken]);
 
   const onLogin = () => {
     auth0.webAuth
@@ -18,9 +54,7 @@ const App = () => {
         audience: 'https://dev-xf5mji86.us.auth0.com/api/v2/',
       })
       .then(credentials => {
-        Alert.alert('AccessToken: ' + credentials.accessToken);
         setAccessToken(credentials.accessToken);
-        console.log('AccessToke: ' + credentials.accessToken);
       })
       .catch(error => console.log(error));
   };
@@ -37,24 +71,85 @@ const App = () => {
       });
   };
 
-  let loggedIn = accessToken !== null;
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}> Auth0Sample - Login </Text>
-      <Text>You are{loggedIn ? ' ' : ' not '}logged in. </Text>
-      <Button
-        onPress={loggedIn ? onLogout : onLogin}
-        title={loggedIn ? 'Log Out' : 'Log In'}
-      />
-    </View>
-  );
+  const createPost = () => {
+    axios
+      .post(
+        'http://localhost:5004/posts/',
+        {
+          title: inputText,
+          body: bodyText,
+          public: true,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      )
+      .then(res => {
+        fetchUserPosts();
+        console.log(res);
+      });
+  };
+
+
+  if (loggedIn) {
+    return (
+      <ScrollView>
+        <View style={{padding: 20}}>
+          <Text h4 style={{marginTop: 50}}>
+            Create post:
+          </Text>
+          <TextInput
+            editable={true}
+            placeholder="Title"
+            onChangeText={text => setInputText(text)}
+            value={inputText}
+            style={styles.input}
+          />
+          <TextInput
+            editable={true}
+            placeholder="Body"
+            onChangeText={text => setBodyText(text)}
+            value={bodyText}
+            style={styles.input}
+          />
+          <Button onPress={createPost} title={'Create'} />
+
+          <Text h4 style={{marginTop: 20}}>
+            Your posts:
+          </Text>
+          <UserPostList posts={userPosts} />
+          <View style={{marginTop: 30}} />
+          <Button onPress={onLogout} title={'Log out'} />
+        </View>
+      </ScrollView>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Please log in</Text>
+        <Button onPress={onLogin} title={'Log in'} />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 60,
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingBottom: 60,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   header: {
